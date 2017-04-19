75efeb653f27d8febd3145e1a7098aaccb11abd3
orcsab <orcsab@gmail.com>
2017-04-19 12:34:16 +0800

    click and reposition

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
 // Javascript 
+
+var eventCount = 0;
+var eventProperty = [];
+
+var TrackMouse = function (mouseEvent) {
+    eventProperty[eventCount++] = {
+        id: mouseEvent.toElement.id,
+        type: 'mouse',
+        ts: Date.now(),
+        x: mouseEvent.x,
+        y: mouseEvent.y
+    };
+
+    console.log("Element id: " + eventProperty[eventCount - 1].id
+    			+ ", X: " + mouseEvent.x
+    			+ ", Y: " + mouseEvent.y
+    			+ "\n");
+
+    var cicle = document.getElementById("circle");
+    console.log("Id: " + circle.id);
+    console.log("Before: " + circle.style.left + " " + circle.style.top);
+    circle.style.left = mouseEvent.x + 'px';
+    circle.style.top = mouseEvent.y + 'px';
+    console.log("After: " + circle.style.left + " " + circle.style.top);
+
+};
+
+document.addEventListener('click', TrackMouse);
